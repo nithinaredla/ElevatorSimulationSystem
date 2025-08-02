2cf71d328a83edc1d0b8d1aae0c4d30df90148b2
ntihinaredla <nithinaredla.professional@gmail.com>
2025-08-03 00:50:11 +0530

    updated frontend

--- front-end/src/api/simulation.ts
+++ front-end/src/api/simulation.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://elevator-simulation-system-ny44t2jgn-nithinaredlas-projects.vercel.app/api/simulation';
+const BASE_URL = 'https://elevator-simulation-system.vercel.app/api/simulation';
 
 export const resetSimulation = (elevators: number, floors: number) => {
   return axios.post(`${BASE_URL}/reset`, { elevators, floors });
--- front-end/src/components/Controls.tsx
+++ front-end/src/components/Controls.tsx
@@ -33,12 +33,12 @@ const Controls: React.FC<Props> = ({  onReset, onFloorChange, speed, setSpeed  }
   };
 
   const handlePause = async () => {
-    await axios.post('https://elevator-simulation-system-ny44t2jgn-nithinaredlas-projects.vercel.app/api/simulation/pause');
+    await axios.post('https://elevator-simulation-system.vercel.app/api/simulation/pause');
     setIsPaused(true);
   };
 
   const handleResume = async () => {
-    await axios.post('https://elevator-simulation-system-ny44t2jgn-nithinaredlas-projects.vercel.app/api/simulation/resume');
+    await axios.post('https://elevator-simulation-system.vercel.app/api/simulation/resume');
     setIsPaused(false);
   };
 
